c070d2e2626ab4061e7a5445ad5887b225925d89
Victor Hostert <victorhostert@gmail.com>
2022-01-04 16:57:48 -0300

    feat: incluídas urls de todos os apps

--- administrador_financas/urls.py
+++ administrador_financas/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path
+from django.urls.conf import include
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.homepage, name='homepage')
+    path('', views.homepage, name='homepage'),
+    path('contas/', include('contas.urls')),
+    path('despesas/', include('despesas.urls')),
+    path('receitas/', include('receitas.urls')),
 ]
