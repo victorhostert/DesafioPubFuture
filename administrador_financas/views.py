f5a645197d4a21e16563ae297ac571ed379a7f23
Victor Hostert <victorhostert@gmail.com>
2022-01-05 18:20:19 -0300

    feat: saldo total agora é exibido na homepage

--- administrador_financas/views.py
+++ administrador_financas/views.py
@@ -3,7 +3,11 @@ from contas.models import Contas
 
 def homepage(request):
     contas = Contas.objects.all()
+    saldo_total = 0
+    for conta in contas:
+        saldo_total += conta.saldo
     context = {
-        'contas': contas
+        'contas': contas,
+        'saldo_total': saldo_total
     }
     return render(request, 'homepage.html', context)
